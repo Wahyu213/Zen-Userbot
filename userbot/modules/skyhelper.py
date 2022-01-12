209422558f6dd3aded25344d86268814421a8e58
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2022-01-12 14:26:18 +0700

    Create skyhelper.py

---
+++ userbot/modules/skyhelper.py
@@ -0,0 +1,34 @@
+""" Userbot module for other small commands. """
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern="^.lhelp$")
+async def usit(e):
+    await e.edit(
+        f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "\n[Telegram](t.me/zenfrans)"
+        "\n[Repo](https://github.com/Wahyu213/Zen-Userbot)"
+    )
+
+
+@register(outgoing=True, pattern="^.vars$")
+async def var(m):
+    await m.edit(
+        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Wahyu213/Zenn-Userbot/Zen-userbot/varshelper.txt)"
+    )
+
+
+CMD_HELP.update(
+    {
+        "helper": "`.lhelp`\
+\nUsage: Bantuan Untuk Zen-Userbot.\
+\n`.vars`\
+\nUsage: Melihat Daftar Vars."
+    }
+)
