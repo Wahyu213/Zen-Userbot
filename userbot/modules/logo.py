f260d062cd6ac0e1e4f941f23a4e5f5a2b145631
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2022-01-13 21:21:19 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -16,7 +16,7 @@ async def _(event):
         await event.edit("`Give a name too!`")
     else:
         await event.edit("`Processing`")
-    chat = "@SkyzuRobot"
+    chat = "@OkaeriRobot"
     async with event.client.conversation(chat) as conv:
         try:
             msg = await conv.send_message(f"/logo {text}")
